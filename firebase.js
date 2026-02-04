0d32a12ea0b326f66d0e5a8d25c72fd0bd98b41d
Arjun <arjun.t963@gmail.com>
2026-02-04 17:17:37 +0530

    configfb

--- firebase.js
+++ firebase.js
@@ -4,8 +4,8 @@ const firebaseConfig = {
   projectId: "physio-connect-1286f",
   storageBucket: "physio-connect-1286f.firebasestorage.app",
   messagingSenderId: "42846524978",
-  appId: "1:42846524978:web:676200350d62602a964e2b",
-  measurementId: "G-WFX4L7EFY6"
+  appId: "1:42846524978:web:2659eb2197c3d156964e2b",
+  measurementId: "G-YDBH4QXJ0F"
 };
 
 firebase.initializeApp(firebaseConfig);
